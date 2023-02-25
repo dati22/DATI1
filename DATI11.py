d9ca8e14d342920d4b3e43166fe38f1cc6a73c40
dati22 <125134310+dati22@users.noreply.github.com>
2023-02-25 17:47:29 +0300

    Add files via upload

---
+++ DATI11.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#ЗАДАНИЕ 2
+a=int(input((a+2)/(b+5))**4)
+b=int(input(0.001))
+c=int(input(c))
+result=((a+2)/(b+5))**4-0.001*c
+
+
+# In[14]:
+
+
+#ЗАДАНИЕ 3
+a=input()
+print('='*39)
+print('\t сообщение: ' + a)
+print('='*39)
+
+
+# In[15]:
+
+
+#задание 4
+a=("lemons,oranges,blueberries")
+b=["lemons","oranges","blueberries"]
+print(a[1])
+print(b[1])
+
+
+# In[17]:
+
+
+a=("lemons,oranges,blueberries")
+b=["lemons","oranges","blueberries"]
+print(a[8])
+print(b[2])
+
+
+# In[21]:
+
+
+#задание 5
+a=[15,78,93,90,26,45]
+del(a[4])
+print(a)
+a.append(17)
+print(a)
+
+
+# In[32]:
+
+
+#задание 6
+a={"china":"pekin","russia":"moscow",italy":"rom","vegetable":"potato"}
+del(a["italy"])
+print(a)
+
+
+# In[33]:
+
+
+#задание 7
+a=int(input)
+b=int(input)
+if (a > b):
+    print("a" больше "b")
+    if (b > a):
+        print("b"больше "b")
+        else
+
+
+# In[36]:
+
+
+a=int(input())
+if (a==1):
+    print("monday")
+if (a==2):
+    print("tueday")
+if (a==3):
+    print("wednesday")
+if (a==4):
+    print("thursday")
+if (a==5):
+    print("friday")
+if (a==6):
+    print("saturday")
+if (a==7):
+    print("sunday")
+
+
+# In[45]:
+
+
+a=int(input())
+print(str(a) + " километра это " + str(a*1000) + " метров")
+
+
+# In[48]:
+
+
+a=int(input())
+b=int(input())
+print(str(a*b) + "это площадь квадарата")
+
+
+# In[47]:
+
+
+a=input()
+print(a.upper())
+
+
+# In[ ]:
+
+
+
+
